fe48f8e48f6c6b96be25f4c6cf1f9e035dace95a
Simone Hoffmann <simohoff@gmx.de>
2020-11-15 10:34:55 +0100

    client server communiction working

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 // import express library, still needs to do that, although installed with npm in terminal
 const express = require("express");
+const cors = require("cors");
 
 // Creating an express application
 const app = express();
+
+// Cors is another middleware that adds those cors headers to it
+app.use(cors());
+
+// Sever needs to understand Json so get express json body parser, everything that has "content-type": "application/json" will be parsed this way
+app.use(express.json());
+
+// Creating home route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Meower! 😆",
+  });
+});
+
+app.post("/mews", (req, res) => {
+  console.log(req.body);
+});
+
+// Listening
+app.listen(5000, () => {
+  console.log("Listening on http://localhost:5000");
+});
