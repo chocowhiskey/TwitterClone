e8938d7b0d78fad137e8c06403af79d91d5157d7
Simone Hoffmann <simohoff@gmx.de>
2020-11-15 10:06:50 +0100

    add loading gif and server dir

---
+++ server/index.js
@@ -0,0 +1,5 @@
+// import express library, still needs to do that, although installed with npm in terminal
+const express = require("express");
+
+// Creating an express application
+const app = express();
