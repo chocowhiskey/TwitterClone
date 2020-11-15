6b120df54bd45eb0532b55815c523f1e834c509f
Simone Hoffmann <simohoff@gmx.de>
2020-11-15 11:35:56 +0100

    áll mews are shown on the page, mongodb working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 // import express library, still needs to do that, although installed with npm in terminal
 const express = require("express");
 const cors = require("cors");
+const monk = require("monk");
 
 // Creating an express application
 const app = express();
+
+// Connect to db
+const db = monk("localhost/meower");
+const mews = db.get("mews");
 
 // Cors is another middleware that adds those cors headers to it
 app.use(cors());
@@ -18,8 +23,40 @@ app.get("/", (req, res) => {
   });
 });
 
+// If client calls the route /mews
+app.get("/mews", (req, res) => {
+  mews.find().then((mews) => {
+    res.json(mews);
+  });
+});
+
+// Validation
+function isValidMew(mew) {
+  return (
+    mew.name &&
+    mew.name.toString().trim() !== "" &&
+    mew.content &&
+    mew.content.toString().trim() !== ""
+  );
+}
+
 app.post("/mews", (req, res) => {
-  console.log(req.body);
+  // Validate input, toString() is required for preventing injections
+  if (isValidMew(req.body)) {
+    const mew = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString(),
+      created: new Date(),
+    };
+    mews.insert(mew).then((createdMew) => {
+      res.json(createdMew);
+    });
+  } else {
+    res.status(422);
+    res.json({
+      message: "Hey! Name and Content are required!",
+    });
+  }
 });
 
 // Listening
